66eed40564c2dcf046e111f83bd0fc4c525c45cf
Krzysztof <krzysiek@woelke.pl>
2023-03-05 11:15:34 +0100

    init structure of web-blog project

---
+++ src/main/java/pl/woelke/springblogwebapp/SpringBlogWebappApplication.java
@@ -0,0 +1,13 @@
+package pl.woelke.springblogwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBlogWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBlogWebappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/woelke/springblogwebapp/SpringBlogWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.woelke.springblogwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBlogWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
